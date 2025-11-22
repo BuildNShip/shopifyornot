3e2122f5a2e523b40a304d192375d26f151cb29a
Aswin Asok <aswinasokofficial@gmail.com>
2025-11-22 22:27:38 +0530

    fix(tags)

--- app/layout.tsx
+++ app/layout.tsx
@@ -89,13 +89,13 @@ export default function RootLayout({
         <html lang="en" className="scroll-smooth">
             <Script
                 id="gtm-script"
-                strategy="afterInteractive"
+                strategy="beforeInteractive"
             >{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-P3MHGHX3');`}</Script>
-            <Script id="clarity-script" strategy="afterInteractive">{`(function(c,l,a,r,i,t,y){
+            <Script id="clarity-script" strategy="beforeInteractive">{`(function(c,l,a,r,i,t,y){
     c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
     t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
     y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
